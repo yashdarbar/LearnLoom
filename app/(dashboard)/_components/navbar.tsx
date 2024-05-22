a21d884f31b9335efcf66118848500bc758e5f6d
Yash Darbar <darbaryash44@gmail.com>
2024-05-22 16:05:09 +0530

    menu button is working

---
+++ app/(dashboard)/_components/navbar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import MobileSidebar from "./mobile-sidebar";
+
+
+const NavBar = () => {
+    return (
+        <div className="p-4 border-b h-full flex items-center bg-white shadow-sm">
+            <MobileSidebar/>
+        </div>
+    );
+};
+
+export default NavBar;
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import SideBar from "./_components/sidebar";
+import NavBar from "./_components/navbar";
 
 const dashBoardLayout = ({ children }: { children: React.ReactNode }) => {
     return (
         <div className="h-full">
-            <div>
-                
+            <div className="h-[80px] w-full md:pl-56 fixed z-50 inset-y-0">
+                <NavBar/>
             </div>
             <div className="hidden md:flex h-full w-56 flex-col fixed z-50 inset-y-0">
                 <SideBar/>
