4816530453ed48af927ace0f1e2e2e6b9c61f81e
Yash Darbar <darbaryash44@gmail.com>
2024-05-27 03:22:50 +0530

    learn how to impliment form will work on it proprely

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -4,6 +4,7 @@ import * as z from "zod";
 import axios from "axios";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
 
 import {
     Form,
@@ -17,10 +18,59 @@ import {
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 
+
+const formSchema = z.object({
+    userName: z.string().min(2, {
+        message: "Username must be at least 2 <characters></characters>",
+    }),
+});
+
 const CreatePage = () => {
+
+  //const form = useForm();
+  const router = useRouter();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+      resolver: zodResolver(formSchema),
+      defaultValues: {
+          userName: "",
+      },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+      console.log(values);
+  }
+
   return (
-    <div>CreatePage</div>
-  )
+      <div>
+          CreatePage
+          <div>
+              <Form {...form}>
+                  <form onSubmit={form.handleSubmit(onSubmit)}>
+                      <FormField
+                          control={form.control}
+                          name="userName"
+                          render={({ field }) => (
+                              <FormItem>
+                                  <FormLabel>Username</FormLabel>
+                                  <FormControl>
+                                      <Input
+                                          placeholder="userName"
+                                          {...field}
+                                      />
+                                  </FormControl>
+                                  <FormDescription>
+                                      This is your public display name.
+                                  </FormDescription>
+                                  <FormMessage />
+                              </FormItem>
+                          )} />
+                      <Button type="submit">Submit</Button>
+                  </form>
+              </Form>
+          </div>
+      </div>
+  );
 }
 
 export default CreatePage
