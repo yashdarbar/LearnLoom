0ccb7514e5ac54070eca5583824191e7c58413b5
Yash Darbar <darbaryash44@gmail.com>
2024-06-08 04:23:18 +0530

    api

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -42,8 +42,8 @@ const CreatePage = () => {
 
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
         try {
-            const response = await axios.post("api/course/", values);
-
+            const response = await axios.post("/api/courses", values);
+            router.push(`/teacher/courses/${response.data.id}`);
         } catch {
             toast.error(
                 "Something went wrong"
