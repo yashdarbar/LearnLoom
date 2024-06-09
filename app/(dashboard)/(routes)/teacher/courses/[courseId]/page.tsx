fc39ebd216b7d0c133fd8956a70e71e3ac08a3fc
Yash Darbar <darbaryash44@gmail.com>
2024-06-09 16:12:28 +0530

    commit

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,7 +1,37 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
+const CourseId = async ({ params }: { params: { courseId: string } }) => {
 
-const CourseId = ({params}: {params : {courseId: string} }) => {
-    return <div>CourseId: {params.courseId}</div>;
+    const { userId } = auth()
+
+    if (!userId) {
+        return redirect("/");
+    }
+
+    const course = await db.course.findUnique({
+        where: {
+            id: params.courseId
+        }
+    })
+
+    if (!course) { return redirect("/"); }
+
+    const allTheFields = [
+        course.title,
+        course.description,
+        course.imageUrl,
+        course.price,
+        course.categoryId,
+    ]
+
+    const totalFields = allTheFields.length;
+    const completedFields = allTheFields.filter(Boolean).length;
+    const completedText = `(${completedFields}/${totalFields})`
+
+    return <div>CourseId: {params.courseId}
+    <div>{completedText}</div></div>;
 };
 
 export default CourseId;
