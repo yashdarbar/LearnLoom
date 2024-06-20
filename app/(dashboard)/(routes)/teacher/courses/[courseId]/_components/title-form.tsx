27ada3be417b6f3c0d89f0b5a3fa90e28fc5a074
Yash Darbar <darbaryash44@gmail.com>
2024-06-21 04:07:37 +0530

    commit

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx
@@ -5,9 +5,16 @@ import axios from "axios";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 
-import { Form, FormControl } from "@/components/ui/form";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { Pencil } from "lucide-react";
 
 interface TitleFormProps {
     initialData: {
@@ -16,8 +23,35 @@ interface TitleFormProps {
     courseId: string;
 }
 
+const formSchema = z.object({
+    title: z.string().min(1, {
+        message: "title is required",
+    }),
+});
+
 const TitleForm = ({ initialData, courseId }: TitleFormProps) => {
-    return <div>TitleForm</div>;
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: initialData,
+    });
+
+    const { isSubmitting, isValid} = form.formState
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        console.log(values);
+    }
+
+    return (
+        <div className="mt-6 border bg-slate-100 rounded-md p-4">
+            <div className="font-medium flex items-center justify-between">
+                Course title
+                <Button variant="ghost">
+                    <Pencil className="h-4 w-4 mr-2 " />
+                    Edit title
+                </Button>
+            </div>
+        </div>
+    );
 };
 
 export default TitleForm;
