827e7b5a8e23886445cffcbc1f4c01d6a8e36510
Yash Darbar <darbaryash44@gmail.com>
2024-07-02 01:38:35 +0530

    toggleedit button

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx
@@ -4,6 +4,7 @@ import * as z from "zod";
 import axios from "axios";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { Pencil } from "lucide-react";
 
 import {
     Form,
@@ -14,9 +15,7 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { Pencil } from "lucide-react";
-
-
+import { useState } from "react";
 
 interface TitleFormProps {
     initialData: {
@@ -37,19 +36,29 @@ const TitleForm = ({ initialData, courseId }: TitleFormProps) => {
         defaultValues: initialData,
     });
 
-    const { isSubmitting, isValid} = form.formState
+    const [isEditing, setIsEditing] = useState(false);
+
+    const { isSubmitting, isValid } = form.formState;
 
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
         console.log(values);
-    }
+    };
+
+    const toggleEdit = () => setIsEditing((current)=>(!current));
 
     return (
         <div className="mt-6 border bg-slate-100 rounded-md p-4">
             <div className="font-medium flex items-center justify-between">
                 Course title
-                <Button variant="ghost">
-                    <Pencil className="h-4 w-4 mr-2 " />
-                    Edit title
+                <Button variant="ghost" onClick={toggleEdit}>
+                    {isEditing ? (
+                        <>Cancel</>
+                    ) : (
+                        <>
+                            <Pencil className="h-4 w-4 mr-2 " />
+                            Edit title
+                        </>
+                    )}
                 </Button>
             </div>
         </div>
@@ -57,39 +66,3 @@ const TitleForm = ({ initialData, courseId }: TitleFormProps) => {
 };
 
 export default TitleForm;
-
-
-// import axios from "axios";
-// import { zodResolver } from "@hookform/resolvers/zod";
-// import * as z from "zod"
-// import { Form, FormField, FormDescription, FormMessage } from "@/components/ui/form";
-// import { Input } from "@/components/ui/input";
-// import { Button } from "@/components/ui/button";
-// import { useForm } from "react-hook-form";
-
-// interface TitleFromProps {
-//     initialData: {
-//         title: string;
-//     };
-//     courseId: string;
-// }
-
-// const formSchema = z.object({
-//     title: z.string().min(1, {
-//         message: "Title required",
-//     }),
-// })
-
-// const TitleForm = ({initialData, courseId}: TitleFromProps) => {
-
-//     const form = useForm<z.infer<typeof formSchema>>({
-//         resolver: zodResolver(formSchema),
-//         defaultValues: initialData
-//     })
-
-//   return (
-//     <div>TitleForm</div>
-//   )
-// }
-
-// export default TitleForm
