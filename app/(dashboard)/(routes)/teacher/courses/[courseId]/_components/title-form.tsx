3a45c6e339ac877f02fea9dfba39339224daf4df
Yash Darbar <darbaryash44@gmail.com>
2024-07-03 01:21:36 +0530

    title form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx
@@ -12,6 +12,7 @@ import {
     FormField,
     FormItem,
     FormMessage,
+    FormLabel,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
@@ -61,6 +62,27 @@ const TitleForm = ({ initialData, courseId }: TitleFormProps) => {
                     )}
                 </Button>
             </div>
+                {!isEditing && (<p className="text-sm mt-2">{initialData.title}</p>)}
+                {isEditing && (
+                    <Form {...form}>
+                        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 mt-4" >
+                            <FormField control={form.control} name="title"
+                            render={({field}) => (<FormItem>
+                                <FormControl>
+                                    <Input disabled={isSubmitting} placeholder="e.g. 'web Development, UI/UX'" {...field}/>
+                                </FormControl>
+                                <FormMessage/>
+                            </FormItem>)}
+                            />
+                            <div className="flex items-center gap-x-2">
+                                <Button disabled={!isValid || isSubmitting}
+                                type="submit">
+                                    Save
+                                </Button>
+                            </div>
+                        </form>
+                    </Form>
+                )}
         </div>
     );
 };
