03950aeb243de0bc0806b521f5816a49e6e319fb
Yash Darbar <darbaryash44@gmail.com>
2024-07-09 23:44:25 +0530

    commit

--- app/api/courses/[courseId]/route.ts
+++ app/api/courses/[courseId]/route.ts
@@ -5,6 +5,11 @@ export async function PATCH(req: Request, {params}: { params: { courseId: string
     try {
         const { userId } = auth();
         const { courseId } = params;
+        const values = await req.json();
+
+        if (!userId) {
+            return new NextResponse("Unauthorized", {status: 401});
+        }
     } catch (error) {
         console.log("[COURES_ID", error);
         return new NextResponse("Internal Error", {status: 500});
