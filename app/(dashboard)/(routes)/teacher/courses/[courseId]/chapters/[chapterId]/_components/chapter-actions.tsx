91813a695be4e116a81b56f17467a798b0f77bce
Yash Darbar <darbaryash44@gmail.com>
2024-08-02 03:56:39 +0530

    chapter delete fuctionality is added

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-actions.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-actions.tsx
@@ -2,7 +2,11 @@
 
 import ConfirmModel from "@/components/models/confirm-model";
 import { Button } from "@/components/ui/button";
+import axios from "axios";
 import { Trash } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import toast from "react-hot-toast";
 
 interface ChapterActionsProps {
     chapterId: string;
@@ -17,21 +21,40 @@ const ChapterActions = ({
     isPublished,
     disabled
 }: ChapterActionsProps) => {
+
+    const router = useRouter();
+
+    const [isLoading, setIsLoading] = useState(false);
+
+    const onDelete = async () => {
+        try {
+            setIsLoading(true);
+            await axios.delete(`/api/courses/${courseId}/chapters/${chapterId}`);
+            toast.success("Chapter deleted");
+            router.refresh();
+            router.push(`/teacher/courses/${courseId}`);
+        } catch {
+        toast.error("Something went wrong");
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
     return (
         <div className="flex items-center gap-x-2">
             <Button
                 size="sm"
                 variant="outline"
-                disabled={disabled}
+                disabled={disabled || isLoading}
                 onClick={() => {}}
             >
                 {isPublished ? "Unpublished" : "Published"}
             </Button>
-            <Button size="sm">
-                <ConfirmModel onConfirm={()=>{}}>
-                    <Trash className="h-4 w-4" />
-                </ConfirmModel>
-            </Button>
+            <ConfirmModel onConfirm={onDelete}>
+                <Button size="sm" disabled={isLoading}>
+                        <Trash className="h-4 w-4" />
+                </Button>
+            </ConfirmModel>
         </div>
     );
 };
--- app/api/courses/[courseId]/chapters/[chapterId]/route.ts
+++ app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -1,4 +1,4 @@
-import  Mux  from "@mux/mux-node"
+import Mux from "@mux/mux-node";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
@@ -10,6 +10,84 @@ const mux = new Mux({
 
 const { video } = mux;
 
+export async function DELETE(
+    req: Request,
+    { params }: { params: { courseId: string; chapterId: string } }
+) {
+    try {
+        const { userId } = auth();
+        if (!userId) {
+            return new NextResponse("Unauthenticated", { status: 401 });
+        }
+        const courseOwner = await db.course.findUnique({
+            where: {
+                id: params.courseId,
+                userId: userId,
+            },
+        });
+
+        if (!courseOwner) {
+            return new NextResponse("Unauthorized", { status: 401 });
+        }
+
+        const chapter = await db.chapter.findUnique({
+            where: {
+                courseId: params.courseId,
+                id: params.chapterId
+            }
+        });
+
+        if(!chapter) {
+            return new NextResponse("Not Found", { status: 404});
+        }
+
+        if (chapter.videoUrl) {
+            const existingMuxData = await db.muxData.findFirst({
+                where: {
+                    chapterId: params.chapterId,
+                }
+            });
+
+            if(existingMuxData) {
+                await video.assets.delete(existingMuxData.assetId);
+                await db.muxData.delete({
+                    where: {
+                        id: existingMuxData.id
+                }});
+            }
+        }
+
+        const deleteChapter = await db.chapter.delete({
+            where: {
+                id: params.chapterId
+            }
+        });
+
+        const publishedChaptersInCourse = await db.chapter.findMany({
+            where: {
+                courseId: params.courseId,
+                isPublished: true
+            }
+        });
+
+        if (!publishedChaptersInCourse) {
+            await db.course.update({
+                where: {
+                    id: params.courseId,
+                },
+                data: {
+                    isPublished: false,
+                }
+            });
+        }
+
+        return NextResponse.json(deleteChapter);
+
+    } catch (error) {
+        console.log("[CHAPTER_DELETE]", error);
+        return new NextResponse("Internal Error", { status: 500 });
+    }
+}
 
 export async function PATCH(
     req: Request,
@@ -17,7 +95,7 @@ export async function PATCH(
 ) {
     try {
         const { userId } = auth();
-        const {isPublised, ...values} = await req.json();
+        const { isPublised, ...values } = await req.json();
 
         if (!userId) {
             return new NextResponse("Unauthorized", { status: 401 });
@@ -38,29 +116,31 @@ export async function PATCH(
             where: {
                 id: params.chapterId,
                 courseId: params.courseId,
-            }, data: {
+            },
+            data: {
                 ...values,
-            }
+            },
         });
 
         if (values.videoUrl) {
             const existingMuxData = await db.muxData.findFirst({
                 where: {
                     chapterId: params.chapterId,
-                }});
+                },
+            });
 
-            if(existingMuxData) {
+            if (existingMuxData) {
                 await video.assets.delete(existingMuxData.assetId);
                 await db.muxData.delete({
                     where: {
                         id: existingMuxData.id,
-                    }});
-                }
+                    },
+                });
+            }
 
             const asset = await video.assets.create({
                 input: values.videoUrl,
                 playback_policy: ["public"],
-                
             });
 
             await db.muxData.create({
@@ -68,8 +148,7 @@ export async function PATCH(
                     assetId: asset.id,
                     chapterId: params.chapterId,
                     playbackId: asset.playback_ids?.[0]?.id,
-
-                }
+                },
             });
         }
 
