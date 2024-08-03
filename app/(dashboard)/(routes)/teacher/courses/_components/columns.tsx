7b666156b73e33344e49fecfafc3652d7f70d63a
Yash Darbar <darbaryash44@gmail.com>
2024-08-03 17:00:04 +0530

    course table

---
+++ app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { Course } from "@prisma/client";
+import { ColumnDef } from "@tanstack/react-table";
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+
+export const columns: ColumnDef<Course>[] = [
+    {
+        accessorKey: "title",
+        header: "Title",
+    },
+    {
+        accessorKey: "price",
+        header: "Price",
+    },
+    {
+        accessorKey: "isPublished",
+        header: "Published",
+    },
+];
--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,15 +1,40 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { DataTable } from "./_components/data-table";
+import { Course } from "@prisma/client";
+import { columns } from "./_components/columns";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
 
 
 
-const Courses = () => {
+
+
+const Courses = async () => {
+
+    const { userId } = auth();
+
+    if (!userId) {
+        redirect("/")
+    }
+
+    const courses = await db.course.findMany({
+        where: {
+            userId: userId,
+        },
+        orderBy: {
+            createdAt: "desc",
+        }
+    });
+
     return (
         <div className="dark:bg-black h-full">
             <div className="p-6">
-                <Link href="/teacher/create">
+                {/* <Link href="/teacher/create">
                     <Button>New Course</Button>
-                </Link>
+                </Link> */}
+                    <DataTable columns={columns} data={courses} />
             </div>
         </div>
     );
