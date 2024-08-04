4c3b1f10a7a5095738f7e37a56cd19260d73868f
Yash Darbar <darbaryash44@gmail.com>
2024-08-05 03:05:51 +0530

    search input(focus visible)

--- components/search-input.tsx
+++ components/search-input.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+"use client";
+
+import { Search } from "lucide-react";
+import { Input } from "./ui/input";
+
 
 const SearchInput = () => {
-  return (
-    <div>SearchInput</div>
-  )
-}
+    return <div className="relative">
+        <Search className="h-4 w-4 top-3 left-3 text-slate-600 absolute"/>
+        <Input className="w-full md:w-[300px] bg-slate-100 pl-9 rounded-full focus-visible:ring-slate-200" placeholder="Search for a course"/>
+    </div>;
+};
 
-export default SearchInput
+export default SearchInput;
