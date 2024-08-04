4950babb73eb9bd454f59c42859cab0a71da0087
Yash Darbar <darbaryash44@gmail.com>
2024-08-05 01:14:09 +0530

    Category searching querry is added

--- app/(dashboard)/(routes)/search/_components/category-item.tsx
+++ app/(dashboard)/(routes)/search/_components/category-item.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import { cn } from "@/lib/utils";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import queryString from "query-string";
+import qs from "query-string";
 import React from "react";
 import { IconType } from "react-icons";
 
@@ -10,9 +14,38 @@ interface CategoryItemProps {
 }
 
 const CategoryItem = ({ label, value, icon: Icon }: CategoryItemProps) => {
+    const pathName = usePathname();
+    const router = useRouter();
+    const searchParams = useSearchParams();
+
+    const currentCategoryId = searchParams.get("categoryId");
+    const currentCategoryTitle = searchParams.get("title");
+
+    const isSelected = currentCategoryId === value;
+
+    const onClick = () => {
+        const url = qs.stringifyUrl(
+            {
+                url: pathName,
+                query: {
+                    title: currentCategoryTitle,
+                    categoryId: isSelected ? null : value,
+                },
+            },
+            { skipEmptyString: true, skipNull: true }
+        );
+
+        router.push(url);
+    };
+
     return (
         <div>
-            <button className="flex items-center border px-3 text-sm hover:border-sky-600 transition py-2 rounded-full gap-x-1">
+            <button onClick={onClick}
+                className={cn(
+                    "flex items-center border px-3 text-sm hover:border-sky-600 transition py-2 rounded-full gap-x-1",
+                    isSelected && "bg-sky-200/20 border-sky-600 text-sky-600"
+                )}
+            >
                 {Icon && <Icon size={20} />}
                 <div className="truncate">{label}</div>
             </button>
