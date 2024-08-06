d2bb8e0add31f32784b41eb1e1f1a46b802cb093
Yash Darbar <darbaryash44@gmail.com>
2024-08-07 01:46:34 +0530

    getCourses function added

--- actions/get-courses.ts
+++ actions/get-courses.ts
@@ -1,28 +1,70 @@
-// import { Course, Category } from "@prisma/client"
-// import { getProgress } from "@/actions/get-progress"
+import { Course, Category } from "@prisma/client"
+import { db } from "@/lib/db"
+import { getProgress } from "@/actions/get-progress"
 
-// type CourseWithProgressWithCategory {
-//     category: Category | null;
-//     chapters: { id: string}[];
-//     progress: number | null;
-// };
+type CourseWithProgressWithCategory = Course & {
+    //catagory: null | Category;
+    chapters: { id: string }[];
+    progress: number | null;
+};
 
-// type GetCourses {
-//     userId: string,
-//     title?: string,
-//     categoryId?: string
-// }
+type GetCourses = {
+    userId: string;
+    title?: string;
+    categoryId?: string;
+}
 
+export const getCourses = async ({userId, title, categoryId}: GetCourses): Promise<CourseWithProgressWithCategory[]> => {
+    try {
+        const courses = await db.course.findMany({
+            where: {
+                isPublished: true,
+                title: {
+                    contains: title
+                },
+                categoryId,
+            },
+            include: {
+                chapters: {
+                    where: {
+                        isPublished: true,
+                    },
+                    select: {
+                        id: true,
+                    }
+                },
+                purchases: {
+                    where: {
+                        userId
+                    }
+                },
 
-// export const getCourses = async ({
-//     userId,
-//     title,
-//     categoryId,
-// }: GetCourses):Promise<CourseWithProgressWithCategory> => {
-//     try {
+            },
+            orderBy: {
+                createdAt: "desc"
+            }
+        });
 
-//     } catch (error) {
-//         console.log("[GET_COURSES]", error);
-//         return [];
-//     }
-// }
+        const courseWithProgress: CourseWithProgressWithCategory[] = await Promise.all(courses.map(async course => {
+            if(course.purchases.length === 0) {
+                return {
+                    ...course,
+                    progress: null
+                }
+            }
+
+            const progressPercentage = await getProgress(userId, course.id);
+
+            return {
+                ...course,
+                progress: progressPercentage,
+            }
+        }));
+
+        return courseWithProgress;
+
+    } catch (error) {
+        console.log("[GET_COURSES]", error);
+        return [];
+    }
+}
